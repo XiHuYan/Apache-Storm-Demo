fede1ab15d362e67d7bcc158a0b96e41d64ef8f6
XiHuYan <3200376951@qq.com>
2019-08-07 16:46:53 +0800

    add kafkaSpout on console input

---
+++ src/main/java/cn/realtime/KafkaTopologyBasic.java
@@ -0,0 +1,68 @@
+package cn.realtime;
+
+import static org.apache.storm.kafka.spout.KafkaSpoutConfig.FirstPollOffsetStrategy.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.kafka.spout.Func;
+import org.apache.storm.kafka.spout.KafkaSpout;
+import org.apache.storm.kafka.spout.KafkaSpoutConfig;
+import org.apache.storm.kafka.spout.KafkaSpoutRetryExponentialBackoff;
+import org.apache.storm.kafka.spout.KafkaSpoutRetryExponentialBackoff.TimeInterval;
+import org.apache.storm.kafka.spout.KafkaSpoutRetryService;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
+
+public class KafkaTopologyBasic {
+
+    private static Func<ConsumerRecord<String, String>, List<Object>> JUST_VALUE_FUNC = new Func<ConsumerRecord<String, String>, List<Object>>() {
+        public List<Object> apply(ConsumerRecord<String, String> record) {
+            return new Values(record.value());
+        }
+    };
+
+    protected KafkaSpoutConfig<String, String> newKafkaSpoutConfig() {
+        return KafkaSpoutConfig.builder("localhost:9092", "test").setProp(ConsumerConfig.GROUP_ID_CONFIG, "kafkaSpoutTestGroup")
+                .setProp(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, 200).setRecordTranslator(JUST_VALUE_FUNC, new Fields("str"))
+                .setRetry(newRetryService()).setOffsetCommitPeriodMs(10000).setFirstPollOffsetStrategy(LATEST)
+                .setMaxUncommittedOffsets(250).build();
+    }
+
+    protected KafkaSpoutRetryService newRetryService() {
+        return new KafkaSpoutRetryExponentialBackoff(new TimeInterval(500L, TimeUnit.MICROSECONDS), TimeInterval.milliSeconds(2),
+                Integer.MAX_VALUE, TimeInterval.seconds(10));
+    }
+
+    public StormTopology buildTopology() {
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("KafkaSpout", new KafkaSpout<String, String>(newKafkaSpoutConfig()), 1);
+        builder.setBolt("SentenceBolt", new SentenceBolt(), 1).globalGrouping("KafkaSpout");
+        builder.setBolt("PrinterBolt", new PrinterBolt(), 1).globalGrouping("SentenceBolt");
+        return builder.createTopology();
+    }
+
+    public static void main(String[] args) {
+        KafkaTopologyBasic kb = new KafkaTopologyBasic();
+        StormTopology topology = kb.buildTopology();
+        LocalCluster cluster = new LocalCluster();
+        Config conf = new Config();
+        cluster.submitTopology("KafkaToplogy", conf, topology);
+        try {
+            // Wait for some time before exiting
+            System.out.println("Waiting to consume from kafka");
+            Thread.sleep(300000);
+        } catch (Exception exception) {
+            System.out.println("Thread interrupted exception : " + exception);
+        }
+        // kill the KafkaTopology
+        cluster.killTopology("KafkaToplogy");
+        // shut down the storm test cluster
+        cluster.shutdown();
+    }
+}
---
+++ src/main/java/cn/realtime/PrinterBolt.java
@@ -0,0 +1,27 @@
+package cn.realtime;
+
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Tuple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PrinterBolt extends BaseBasicBolt {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final Logger logger = LoggerFactory.getLogger(PrinterBolt.class);
+
+    public void execute(Tuple input, BasicOutputCollector collector) {
+        // get the sentence from the tuple and print it
+        String sentence = input.getString(0);
+        logger.info("Received Sentence: " + sentence);
+        System.out.println("Received Sentence: " + sentence);
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        // we don't emit anything
+    }
+
+}
